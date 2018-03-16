ee80e740b8efac02d77e673961e7b3670db43bf7
Henry Hernandez <henry_hernandez74@outlook.com>
2018-03-15 19:44:48 -0700

    Add files via upload

---
+++ Henry Hernandez W3 POS409 Student_Scores/Henry Hernandez W3 POS409 Student_Scores/Form1.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace Henry_Hernandez_W3_POS409_Student_Scores
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.openFile = new System.Windows.Forms.Button();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.resetButton = new System.Windows.Forms.Button();
+            this.openFD = new System.Windows.Forms.OpenFileDialog();
+            this.saveFD = new System.Windows.Forms.SaveFileDialog();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // openFile
+            // 
+            this.openFile.Location = new System.Drawing.Point(334, 85);
+            this.openFile.Name = "openFile";
+            this.openFile.Size = new System.Drawing.Size(75, 23);
+            this.openFile.TabIndex = 0;
+            this.openFile.Text = "Open File";
+            this.openFile.UseVisualStyleBackColor = true;
+            this.openFile.Click += new System.EventHandler(this.openFile_Click);
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(334, 141);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(75, 23);
+            this.saveButton.TabIndex = 1;
+            this.saveButton.Text = "Save File";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // resetButton
+            // 
+            this.resetButton.Location = new System.Drawing.Point(334, 197);
+            this.resetButton.Name = "resetButton";
+            this.resetButton.Size = new System.Drawing.Size(75, 23);
+            this.resetButton.TabIndex = 3;
+            this.resetButton.Text = "Reset";
+            this.resetButton.UseVisualStyleBackColor = true;
+            this.resetButton.Click += new System.EventHandler(this.resetButton_Click);
+            // 
+            // openFD
+            // 
+            this.openFD.FileName = "openFileDialog1";
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(136, 33);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(172, 290);
+            this.listBox2.TabIndex = 5;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 33);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(118, 290);
+            this.listBox1.TabIndex = 6;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(442, 33);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(181, 381);
+            this.textBox1.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.label1.Location = new System.Drawing.Point(12, 344);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(147, 23);
+            this.label1.TabIndex = 9;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(174, 344);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(106, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Class Average Score";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(174, 391);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(114, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Highest Score Student";
+            // 
+            // label4
+            // 
+            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label4.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.label4.Location = new System.Drawing.Point(12, 391);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(147, 23);
+            this.label4.TabIndex = 11;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(40, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(40, 13);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Scores";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(174, 9);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(80, 13);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Student Names";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(517, 9);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(44, 13);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "Edit File";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(639, 425);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.resetButton);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.openFile);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Student Score Records ";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button openFile;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.Button resetButton;
+        private System.Windows.Forms.OpenFileDialog openFD;
+        private System.Windows.Forms.SaveFileDialog saveFD;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ Henry Hernandez W3 POS409 Student_Scores/Henry Hernandez W3 POS409 Student_Scores/Form1.cs
@@ -0,0 +1,120 @@
+﻿
+//Henry Hernandez W3 POS 409 Student Score Records
+//C# Program - Calculates average score, displays highest scoring student, and allows user to edit and save files
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+using System.IO;
+using System.Windows.Forms;
+
+
+namespace Henry_Hernandez_W3_POS409_Student_Scores
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private int studentScores;//private class for sorting open files
+        private const int ARRAY_SIZE = 10;
+        private int[] score = new int[ARRAY_SIZE];
+        string[] name = new string[ARRAY_SIZE];
+        private void openFile_Click(object sender, EventArgs e)
+        {
+        try//captures error
+            {
+                if (openFD.ShowDialog() == DialogResult.OK)//verifies file existence 
+                {
+                    
+                    StreamReader tRead = new StreamReader(openFD.FileName);//opens file
+
+                    while (tRead.Peek() != -1)
+                    {
+                        if (studentScores <= name.Length)//velidates array size
+                        {
+                            score[studentScores] = Convert.ToInt32(tRead.ReadLine());//sorts and collects textfile number data 
+                            name[studentScores] = tRead.ReadLine();//sorts and collects textfile word data
+                            studentScores++;
+                         
+                        }
+                        else
+                        {
+                            MessageBox.Show("Error: Unable To Open File");//error display
+                            break;
+
+
+                        }
+                    }
+                    displayArrays();//calls method to display arrays 
+                    tRead.Close();//Closes the writer 
+                    tRead.Dispose();//Disposes of writer
+                }
+            }
+        catch (System.Exception excep)//error display
+        {
+
+        MessageBox.Show(excep.Message);
+
+        }
+           
+        }
+void displayArrays()
+    {
+       
+            StreamReader tRead = new StreamReader(openFD.FileName);//opens text file
+            textBox1.Text = tRead.ReadToEnd();//reads text file 
+          
+            int answer = (score[0] + score[1] + score[2] + score[3] + score[4] + score[5] + score[6] + score[7] + score[8] + score[9]);
+            int average = answer / 10;
+            string myAvg = average.ToString();//converts int to string
+            label1.Text = myAvg;
+            for (int i = 0; i < studentScores; i++)//For loop that sorts arrays into fields
+            {
+             
+                Array.Sort(score, name);//sorts arrays from least to greatest(descending)
+           
+                listBox1.Items.Add(score[i].ToString());//adds items from text file to list box
+                listBox2.Items.Add(name[i]);//adds items from text file to list box
+                label4.Text = name[9] + score[9];//displays highest scoring student
+           }
+            tRead.Close();//Closes the writer 
+            tRead.Dispose();//Disposes of writer
+    }
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            {
+                //Saves "Edit File" to anhy file
+                if (saveFD.ShowDialog() == DialogResult.OK)//Verifies if savefiledialog is true
+                {
+                    StreamWriter objWriter = new StreamWriter(saveFD.FileName);
+                    objWriter.Write(textBox1.Text);
+                    objWriter.Close();//Closes the writer 
+                    objWriter.Dispose();//Disposes of writer
+                   
+                }
+            }
+        }
+
+        private void resetButton_Click(object sender, EventArgs e)
+        {
+           // Resets all the fields
+            listBox1.Items.Clear();
+            listBox2.Items.Clear();
+            textBox1.Clear();
+            label1.Text="";
+            label4.Text = "";
+        
+        }
+    }
+    
+}
+
